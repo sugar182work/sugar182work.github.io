aeea10c5ee699c30a8d8236605bc2b6f1a33ebcc
sugar182work <sugar182work@gmail.com>
2020-05-04 09:07:44 +0900

    #4

--- components/Top.vue
+++ components/Top.vue
@@ -2,7 +2,7 @@
   <v-parallax dark src="https://picsum.photos/seed/picsum/1920/1080?grayscale">
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
-        <h1 class="display-1 font-weight-thin mb-4">Masanori Sato</h1>
+        <h1 class="display-1 font-weight-thin mb-4">ZuckerSystem</h1>
         <h4 class="subheading">
           Love is like a flower – you’ve got to let it grow.
         </h4>
